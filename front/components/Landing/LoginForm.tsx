fff0a3f3cc1bac53baacf190167d32b0bcbb669c
mirrer <alsejr1004@gmail.com>
2024-01-30 15:19:07 +0900

    Feat: 로그인 폼 마크업, 커스텀 훅

--- front/components/Landing/LoginForm.tsx
+++ front/components/Landing/LoginForm.tsx
@@ -1,9 +1,57 @@
-import React from 'react';
+import React, { useCallback } from 'react';
+import { GoogleCircleFilled } from '@ant-design/icons';
+
+import useInput from 'hooks/useInput';
 
 const LoginForm = () => {
+  const [email, onChangeEmail] = useInput('');
+  const [password, onChangePassword] = useInput('');
+  const [rememberMe, onChangeRememberMe] = useInput(false);
+
+  const onSubmitForm = useCallback(
+    (e: React.FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
+
+      console.log(email, password, rememberMe);
+    },
+    [email, password, rememberMe]
+  );
+
   return (
     <section>
-      <h1>Welcome back!</h1>
+      <div>
+        <GoogleCircleFilled />
+        <button type="button">Sign in with Google</button>
+      </div>
+
+      <div>
+        <span>OR</span>
+      </div>
+
+      <form onSubmit={onSubmitForm}>
+        <div>
+          <input type="text" value={email} onChange={onChangeEmail} required />
+          <label>Email address</label>
+          <span></span>
+        </div>
+
+        <div>
+          <input type="password" value={password} onChange={onChangePassword} required />
+          <label>Password</label>
+          <span></span>
+        </div>
+
+        <div>
+          <div>
+            <label htmlFor="remember-me">Remember me</label>
+            <input type="checkbox" id="remember-me" checked={rememberMe} onChange={onChangeRememberMe} />
+          </div>
+
+          <button type="button">Forget your password?</button>
+        </div>
+
+        <button type="submit">Log in</button>
+      </form>
     </section>
   );
 };
